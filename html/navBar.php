ed5c209e7db49e6662e76d79c4f3b7d17c9c0bc1
mattwolter <matt.s.wolter@gmail.com>
2016-10-08 08:37:56 -0400

    added recommended list

--- html/navBar.php
+++ html/navBar.php
@@ -10,6 +10,9 @@ echo '<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
       <a class="nav-link" href="events_list.php">Events</a>
     </li>
     <li class="nav-item">
+      <a class="nav-link" href="recommended_list.php">Recommended For You</a>
+    </li>
+    <li class="nav-item">
       <a class="nav-link" href= "user_type.php">Sign Up</a>
     </li>
     <li class="nav-item">
