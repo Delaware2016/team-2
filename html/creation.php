93b4884747ad0a5136f94f6c0b9d8b693e74975b
David Geron-Neubauer <davidgn@udel.edu>
2016-10-08 04:01:03 -0400

    Testing getting user info, putting into both tables

--- html/creation.php
+++ html/creation.php
@@ -13,25 +13,40 @@ $dbname = "myDatabase";
 	    die("Connection failed: " . $conn->connect_error);
 	} 
 
-	$sql = "INSERT INTO User (userID, username, password, userType) VALUES (DELAULT, $_POST['username'], $_POST['password'], 0)";
-	$userID = "SELECT userID FROM User ON (username = $_POST['username'])";
-	$userID = $conn->query($userID);
-	$sql2 = "INSERT INTO Person (userID, locationZip, age) VALUES ($userID, , $_POST['age'])";
-	//note also need to insert into Person/Organization
+	$sql = "INSERT INTO User (userID, username, password, userType) VALUES (int(20) NOT NULL auto_increment, $_POST['username'], $_POST['password'], 0)";
 	$result = $conn->query($sql);
+	//check if worked
 
+	$userIDsql = "SELECT userID FROM User ON (username = $_POST['username'])"; //get userid so can put rest of info in person
+	$result = $conn->query($userIDsql);
+	$result = $result->fetch_assoc();
+	$userID = $result['userID'];
+	//check if worked
+	
+	$sql2 = "INSERT INTO Person (userID, locationZip, age) VALUES ($userID, int(6) NOT NULL auto_increment, $_POST['age'])";
+		//change zip to field once it's added
+		//insert rest of info in Person
 
 
 	if ($result) {
 	    echo "Success!`";
+	    echo "<hr>";
+	    while ($userID) {
+		    echo " userID = " . $row['userID'] . "<br>";
+		}
+	}
+	/*
+	if ($result) {
+	    echo "Success!`";
 
 	    while ($row = $result->fetch_assoc()) {
-	    	echo "in!";
-		    echo " id = " . $row['userID'] . "\n";
+	    	echo "<hr>";
+		    echo " userID = " . $row['userID'] . "<br>";
 		}
 	} else {
 	    echo "Error: " . $sql . "<br>" . $conn->error;
 	}
+	*/
 
 	$conn->close();
 
