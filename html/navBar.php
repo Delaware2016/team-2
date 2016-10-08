b2bbdbf7ab8852c0485e7332f6162a14482f3b03
Johnny Rutkowski <johnnypr@udel.edu>
2016-10-08 07:13:26 -0400

    php include navbar in welcome.php

---
+++ html/navBar.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
+  <a class="navbar-brand" href="#">Accelerate Delaware</a>
+  <ul class="nav navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Organizations</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Events</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href= "or.html">Sign Up</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href= "login.html">Log In</a>
+    </li>
+  </ul>
+</nav>';
+
+?>
