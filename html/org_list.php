0b39c3236872b2d6446669b58e01baa78ee1a7d2
Johnny Rutkowski <johnnypr@udel.edu>
2016-10-08 07:49:05 -0400

    adding more includes

--- html/org_list.php
+++ html/org_list.php
@@ -1,41 +1,16 @@
-<html>
-<head>
-<meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <link rel="icon" href="favicon.ico">
-
-      <title>Accelerate Delaware</title>
-
-      <!-- Bootstrap core CSS -->
-      <link href="dist/css/bootstrap.min.css" rel="stylesheet">
-
-      <!-- Custom styles for this template -->
-      <link href="dist/css/starter-template.css" rel="stylesheet">
-<script>
-function show(shown, hidden) {
-  document.getElementById(shown).style.display='block';
-  document.getElementById(hidden).style.display='none';
-  return false;
-}
-</script>
-</head>
-<body>
-
+<?php include 'top.php'; ?>
 <?php include 'navBar.php'; ?>
 
+<div class="container">
+
+  <div class="starter-template">
     -ORGANIZATIONS LIST-
 		<br>
     <b> Organizations </b> | <a href= "list.html"> Recommended | </a>
 		<a href= "map.html"> Map</a>
 
 <!-- put business list here -->
+</div>
+</div>
 
-<br>
-<a href= "welcome.html"> Quit</a>
-
-</body>
-</html>
+<?php include 'bottom.php' ?>
