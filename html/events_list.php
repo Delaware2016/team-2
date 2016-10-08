87bd205ba0cafd814fe9b14bb8bd02f884a31bba
mattwolter <matt.s.wolter@gmail.com>
2016-10-08 07:51:22 -0400

    updated list names

---
+++ html/events_list.php
@@ -0,0 +1,16 @@
+<?php include 'top.php'; ?>
+<?php include 'navBar.php'; ?>
+
+<div class="container">
+
+  <div class="starter-template">
+    -ORGANIZATIONS LIST-
+		<br>
+    <b> Organizations </b> | <a href= "list.html"> Recommended | </a>
+		<a href= "map.html"> Map</a>
+
+<!-- put business list here -->
+</div>
+</div>
+
+<?php include 'bottom.php' ?>
