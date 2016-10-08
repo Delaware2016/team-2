91997379cde2ab5d2714c6340d3e913dd5968281
David Geron-Neubauer <davidgn@udel.edu>
2016-10-08 09:57:48 -0400

    Added list population! Will test now

--- html/org_list.php
+++ html/org_list.php
@@ -1,3 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "team2";
+$dbname = "mydb";
+?>
+
 <?php include 'top.php'; ?>
 <?php include 'navBar_user.php'; ?>
 
@@ -10,6 +17,39 @@
 		<a href= "map.html"> Map</a>
 
 <!-- put business list here -->
+	
+	<?php
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		    echo "Connection failed";
+		}
+		
+		//echo "test";
+		$sql = "SELECT * FROM Organization";
+		$result = $conn->query($sql);
+		if($result) {
+			echo "<ul>";
+			while($row = $result->fetch_assoc()) {
+				echo "<li>";
+		        echo "Name: " . $row["orgName"] . "<br>";
+		        echo "Address: " . $row["orgStreet"] . "<br>";
+		        echo $row["orgCity"] . ", " . $row["orgState"] . " " .  $row["orgZip"] . "<br>";
+		        //echo "<hr>";
+		        echo "</li>";
+	    	}
+	    	echo "</ul>";
+	    }
+		//check if worked
+		else {
+		    echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+
+		$conn->close();
+	?>
+
 </div>
 </div>
 
