7ad7607ed65def8bb25aa61417f4fb50c39f4b47
Johnny Rutkowski <johnnypr@udel.edu>
2016-10-08 09:23:25 -0400

    asdf

--- html/navBar.php
+++ html/navBar.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 
 echo '<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
@@ -13,28 +13,5 @@ echo '<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
   </ul>
 </nav>';
 
-
-<?php
-
-echo '<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
-  <a class="navbar-brand" href="welcome.php">Accelerate Delaware</a>
-  <ul class="nav navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="org_list.php">Organizations</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="events_list.php">Events</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="recommended_list.php">Recommended For You</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href= "user_type.php">Sign Up</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href= "login.php">Log In</a>
-    </li>
-  </ul>
-</nav>';
 
 ?>
