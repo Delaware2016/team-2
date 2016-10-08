0788021a62dba84d5d11d5abc496859a66a9168c
David Geron-Neubauer <davidgn@udel.edu>
2016-10-08 00:38:05 -0400

    Added some working points

---
+++ phpPlayground.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>	
+	<head>
+		<meta type="author" value="me"/>
+		<title>PHP Playground</title>
+	</head>
+	
+	<body>
+		<?php echo '<p>Hello World</p><br>'; ?> 
+		<?php echo "<div>test</div><br>"; ?>
+		<?php echo phpinfo();
+		?>
+	
+
+		<?php
+		echo $_SERVER['HTTP_USER_AGENT'];
+		?>
+
+	</body>
+</html>
