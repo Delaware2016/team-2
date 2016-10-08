12debbacd145b5e73b7aa6de88323a685ea797c3
David Geron-Neubauer <davidgn@udel.edu>
2016-10-08 08:29:18 -0400

    minor comment fix, todo: add redirect

--- html/creation_helper.php
+++ html/creation_helper.php
@@ -8,11 +8,6 @@ $dbname = "mydb";
 
 <html>
 <body>
-
-	Your first name is: <?php echo $_POST["firstname"]; ?><br>
-	Your last name is: <?php echo $_POST["lastname"]; ?>
-	<br><br>
-
 	<?php
 		
 		// Create connection
