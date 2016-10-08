95d3cd7c7b5e0567a569dd0f5b47a4d7322c1e60
Johnny Rutkowski <johnnypr@udel.edu>
2016-10-08 07:40:11 -0400

    more includes

---
+++ html/top.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="favicon.ico">
+
+      <title>Accelerate Delaware</title>
+
+      <!-- Bootstrap core CSS -->
+      <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+      <!-- Custom styles for this template -->
+      <link href="dist/css/starter-template.css" rel="stylesheet">
+      <script>
+        function show(shown, hidden) {
+          document.getElementById(shown).style.display='block';
+          document.getElementById(hidden).style.display='none';
+          return false;
+        }
+    </script>
+    </head>
+
+    <body>
